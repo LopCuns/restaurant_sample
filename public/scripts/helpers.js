635f9894ffc7235ed75ec79a524f4019fa4fd490
LopCuns <jorgelopezcuns@gmail.com>
2023-01-05 17:55:57 +0100

    new design , slides js

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -5,5 +5,9 @@ const h = {
     ev: (el,evType,fn) => el.addEventListener(evType,fn),
     getQuery: (el,selector) => el.querySelector(selector),
     getAllQuery : (el,selector) => el.querySelectorAll(selector),
-    toggle : (el,className) => el.classList.toggle(className)
+    toggle : (el,className) => el.classList.toggle(className),
+    toggleBetween:(el1,el2,className) =>{
+        h.toggle(el1,className)
+        h.toggle(el2,className)
+    }
 }
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,12 +5,33 @@ function init(){
     const $platos = h.$('platos'),
     $menuBtn = h.$('menuBtn'),
     $menu = h.$('menu'),
-    $body = document.body
+    $body = document.body,
+    $slides = h.$('slides')
 
     const toggleMenu = () => {
         h.toggle($menu,'ul--opened')
         h.toggle($body,'body--ul--opened')
+    },
+    menuMobile = (e) =>{
+        if(!e.target.classList.contains('nav__ul__li__a')) return
+            toggleMenu()
+    },
+    actualSlide = () => Number($slides.dataset.showing),
+    updateSlide = () => {
+        const newIndex = actualSlide() + 1
+        $slides.dataset.showing = newIndex > 3?1:newIndex 
+    },
+    toggleSlides = () =>{
+        h.toggle($slides,'slide--transition')
+        const previousIndex = actualSlide()
+        updateSlide()
+        const actualIndex = actualSlide()
+        h.toggleBetween(h.$(`slide${actualIndex}`),h.$(`slide${previousIndex}`),'slide--hidden')
+        h.toggleBetween(h.$(`dot${actualIndex}`),h.$(`dot${previousIndex}`),'dot--actual')
     }
+
+    
+
     h.ev($platos,'click',(e)=>{
         if(!e.target.dataset.open) return
         h.toggle(h.getQuery(e.target,'.carta__platos__opener'),'opener--opened')
@@ -21,10 +42,21 @@ function init(){
         toggleMenu()
     })
 
-    h.ev($menu,'click',(e)=>{
-        if(!e.target.classList.contains('nav__ul__li__a')) return
-        toggleMenu()
+    h.ev(window,'resize',()=>{
+        if(window.innerWidth < 1200){
+            h.ev($menu,'click',menuMobile)
+        }
+        else{
+            $menu.removeEventListener('click',menuMobile)
+        }
     })
+    
+    h.getAllQuery(document,'[data-open]').forEach(el=>el.click())
+
+    setInterval(()=>{
+
+        toggleSlides()
+    },6000)
 
 }
 
